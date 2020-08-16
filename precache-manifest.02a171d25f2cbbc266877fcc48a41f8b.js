18a2d5284df8fe18c15a57efdfd9e65c4b650baf
TeamBlessingOfficial <69757137+TeamBlessingOfficial@users.noreply.github.com>
2020-08-16 22:00:40 +0800

    deploy

---
+++ precache-manifest.02a171d25f2cbbc266877fcc48a41f8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846b69d70a66a32969b6ae37d855391c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cab184d3971600953053",
+    "url": "/static/css/main.8a2a56b9.chunk.css"
+  },
+  {
+    "revision": "cd5e55b84362c0fce8cf",
+    "url": "/static/js/2.8fdd8ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fdd8ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab184d3971600953053",
+    "url": "/static/js/main.a07d3f4d.chunk.js"
+  },
+  {
+    "revision": "747324ee76320dc5f6a5",
+    "url": "/static/js/runtime-main.055de3d0.js"
+  },
+  {
+    "revision": "549735aa49e155db012054e12d0f8cec",
+    "url": "/static/media/bg.549735aa.jpg"
+  },
+  {
+    "revision": "788b89b2dc764a18e66fbec346c51116",
+    "url": "/static/media/logo.788b89b2.svg"
+  }
+]);
